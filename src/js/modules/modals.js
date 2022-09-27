81d560676072a262949e64b1d7b577503f6d5417
khobotovdaniil <dinich44@gmail.com>
2022-09-27 11:14:40 +0400

    add show modal by time

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,32 +1,48 @@
 const modals = () => {
-    function bindModal (trigger, modal, close) {
-        trigger.addEventListener('click', (e) => {
-            if (e.target) {
-                e.preventDefault();
-            }
+    function bindModal (triggerSelector, modalSelector, closeSelector) {
+        const trigger = document.querySelectorAll(triggerSelector),
+              modal = document.querySelector(modalSelector),
+              close = document.querySelector(closeSelector);
 
-            modal.style.display = "block";
-            document.body.style.overflow = "hidden";
+        trigger.forEach(item => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+    
+                modal.style.display = "block";
+                // document.body.classList.add('modal-open');
+                document.body.style.overflow = "hidden";
+            });
         });
 
         close.addEventListener('click', () => {
             modal.style.display = "none";
+            // document.body.classList.remove('modal-open');
             document.body.style.overflow = "";
         });
 
         modal.addEventListener('click', (e) => {
             if (e.target === modal) {
                 modal.style.display = "none";
+                // document.body.classList.remove('modal-open');
                 document.body.style.overflow = "";
             }
         });
     }
 
-    const callEngineerBtn = document.querySelector('.popup_engineer_btn'),
-          modalEngineer = document.querySelector('.popup_engineer'),
-          modalEngineerClose = document.querySelector('.popup_engineer .popup_close');
-    
-    bindModal(callEngineerBtn, modalEngineer, modalEngineerClose);
+    function showModelByTime (selector, time) {
+        setTimeout(() => {
+            document.querySelector(selector).style.display = 'block';
+            document.body.style.overflow = "hidden";
+        }, time);
+    }
+
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+
+    bindModal('.phone_link', '.popup', '.popup .popup_close');
+
+    showModelByTime('.popup', 3000);
 };
 
 export default modals;
